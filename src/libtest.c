f1872cacd7c6a87248f7a90b2d6df6139063b3e3
dasdias <das.dias6@gmail.com>
2022-02-28 20:02:43 +0000

    Change in ignore file

--- src/libtest.c
+++
@@ -1,8 +0,0 @@
-#include "sformat.h"
-
-int main(void)
-{
-    printf("This is a %s %s !\n", "formatted", "string");
-    char* s = (char*) sformat("This is also a %s %s !! \n", "formatted", "string");
-    printf("%s",s);
-}
